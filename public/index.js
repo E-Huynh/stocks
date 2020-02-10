ec4c4c41de6bd9a2361a5ab9d3ac7c053e6a3322
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-02-09 21:56:02 -0700

    getSearchedStocked functioning

--- public/index.js
+++ public/index.js
@@ -3,14 +3,18 @@ const key = 'Tpk_34cea288c0494864ae04a08d5ad02dc2'
 //Jquery targets
 const $symbolSearch = $("#symbolSearch");
 const $symbolSearchBtn = $("#symbolSearchBtn");
-
-//call fx
+const $stockDisplayHeader = $("#stockDisplayHeader");
 
 //Functions
 function getSearchedStock(obj) {
-    console.log("obj ", obj);
-    let stockObj = obj;
-    console.log("stockObj: ", stockObj);
+    console.log("obj: ", obj);
+    let arr = [];
+    for (const key in obj) {
+        arr.push(obj[key].quote.symbol)
+    }
+    console.log(arr);
+    //obj manipulation here
+    $stockDisplayHeader.text(obj[arr[0]].quote.symbol);
 };
 
 //API call for a single or multiple stocks
@@ -18,17 +22,19 @@ function getStocks(APIkey) {
     let stock = $symbolSearch.val();
     return new Promise((resolve, reject) => {
         $.ajax({
-            url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${stock}&types=quote&token=${ APIkey }`,
+            url: `https://sandbox.iexapis.com/stable/stock/market/batch?symbols=${stock}&types=quote&token=${APIkey}`,
             method: 'GET'
-        }).then(function(response) {
+        }).then(function (response) {
+            console.log(response);
             return resolve(response);
         })
-        .catch(err => {
-            return reject(err.responseText);
-        })
+            .catch(err => {
+                return reject(err.responseText);
+            })
     })
 };
 //fx handles the ajax call before using data in 2nd fx
+//duplicate this and change 2nd fx to manipulate obj differently
 async function init() {
     let stockObj = await getStocks(key);
     getSearchedStock(stockObj);
